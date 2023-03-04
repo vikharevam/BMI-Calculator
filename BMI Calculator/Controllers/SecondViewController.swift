082be5d234fdc1e4fc0e7b53d52ebcdc78d1ba22
vikharevam <103068846+vikharevam@users.noreply.github.com>
2023-03-04 13:02:15 +0300

    Добавлен новый ViewConttroller

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Александр Вихарев on 04.03.2023.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
