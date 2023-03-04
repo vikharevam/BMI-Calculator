7c27e271680fb46632d838ee256f881852ad0703
vikharevam <103068846+vikharevam@users.noreply.github.com>
2023-03-04 12:45:48 +0300

    Добавление структуры MVC

---
+++ BMI Calculator/Controllers/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Александр Вихарев on 13.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+   
+    
+    @IBAction func userSelectHeight(_ sender: UISlider) {
+        heightLabel.text = " \(String(format: "%.2f", sender.value)) м"
+        
+    }
+    
+    
+    @IBAction func userSelectWeight(_ sender: UISlider) {
+        weightLabel.text = " \(String(format: "%.1f", sender.value)) кг"
+ 
+    }
+    
+    
+    @IBAction func calculatePassedButton(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        let bmi = weight/(height*height)
+        print(bmi)
+        
+        
+    }
+    
+    
+    
+    
+}
